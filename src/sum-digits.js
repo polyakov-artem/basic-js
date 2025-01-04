4568c81115badef7ab385dbf3ba4a5bbb695f51b
polyakov-artem <polyakov.contact@yandex.ru>
2025-01-04 20:12:03 +0300

    feat: implement getSumOfDigits

--- src/sum-digits.js
+++ src/sum-digits.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given a number, replace this number with
@@ -12,11 +12,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSumOfDigits(n) {
+  const str = String(n);
+
+  if (str.length === 1) {
+    return n;
+  }
+
+  return getSumOfDigits(
+    str.split("").reduce((total, current) => total + Number(current), 0)
+  );
 }
 
 module.exports = {
-  getSumOfDigits
+  getSumOfDigits,
 };
